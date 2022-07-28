59ea6080241342c4b1d885c66c8db4e60c8b9c03
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-07-28 10:09:48 -0600

    Moved style stuff into its own file

--- generic programming.py
+++ generic programming.py
@@ -40,70 +40,6 @@ def wait_suggestion_closing_delay():
     actions.sleep(f'{suggestion_closing_delay.get()}ms')
 
 
-flow_control_parentheses_style = mod.setting(
-    'generic_programming_flow_control_parentheses_style',
-    type = str,
-    default = 'left',
-    desc = 'The spacing style to use for flow control parentheses',
-)
-
-function_parentheses_style = mod.setting(
-    'generic_function_parentheses_style',
-    type = str,
-    default = '',
-    desc = 'The spacing style to use for function parentheses',
-)
-
-function_call_parentheses_style = mod.setting(
-    'generic_function_call_parentheses_style',
-    type = str,
-    default = '',
-    desc = 'The spacing style to use for function call parentheses',
-)
-
-
-def apply_spacing_style_to(style, target):
-    space_before = ''
-    space_after = ''
-    if 'left' in style:
-        space_before = ' '
-    if 'right' in style:
-        space_after = ' '
-    if 'both' in style:
-        space_before = ' '
-        space_after = ' '
-    return f'{space_before}{target}{space_after}'
-
-def apply_spacing_style_to_container(style, container):
-    result = apply_spacing_style_to_container_inside(style, container)
-    result = apply_spacing_style_to(style, result)
-    return result
-
-def apply_spacing_style_to_empty_container(style, container):
-    result = apply_spacing_style_to_container(style, container)
-    if 'pad' in style:
-        result = get_container_with_padding_reduced_to_single_space(result)
-    return result
-def get_container_with_padding_reduced_to_single_space(container):
-    return container.replace('  ', ' ')
-
-def apply_spacing_style_to_container_inside(style, container):
-    start = container.get_start()
-    ending = container.get_end()
-    middle = ''
-    if 'pad' in style:
-        middle = '  '
-    return start + middle + ending
-
-def enter_stylized_container_from_right(container: str):
-    actions.edit.left()
-    #If padding within the container, go left a second time
-    if '  ' in container:
-        actions.edit.left()
-    #If the container are followed by a space, go left again
-    if container.endswith(' '):
-       actions.edit.left()
-
 @mod.action_class
 class Actions:
     def generic_programming_get_line() -> str:
@@ -127,44 +63,8 @@ class Actions:
         '''Uses split string ignoring standard containers with standard containers'''
         return split_string_ignoring_containers(text, separator, construct_standard_programming_containers())
 
-    def generic_programming_get_flow_control_parentheses() -> str:
-        '''returns parentheses with the flow control parentheses style'''
-        parentheses_container = get_parentheses_container()
-        stylized_parentheses = apply_spacing_style_to_container(flow_control_parentheses_style.get(), parentheses_container)
-        return stylized_parentheses
-    def generic_programming_insert_flow_control_parentheses():
-        '''inserts parentheses with the flow control parentheses style'''
-        actions.insert(actions.user.generic_programming_get_flow_control_parentheses())
-    def generic_programming_enter_flow_control_parentheses_from_right():
-        '''enters the flow control parentheses if called after inserting them'''
-        enter_stylized_container_from_right(actions.user.generic_programming_get_flow_control_parentheses())
 
- 
-    def generic_programming_get_function_parentheses() -> str:
-        '''returns parentheses with the function parentheses style'''
-        parentheses_container = get_parentheses_container()
-        stylized_parentheses = apply_spacing_style_to_container(function_parentheses_style.get(), parentheses_container)
-        return stylized_parentheses
-    def generic_programming_insert_function_parentheses():
-        '''inserts parentheses with the function parentheses style'''
-        actions.insert(actions.user.generic_programming_get_function_parentheses())
-    def generic_programming_enter_function_parentheses_from_right():
-        '''enters the function parentheses if called after inserting them'''
-        enter_stylized_container_from_right(actions.user.generic_programming_get_function_parentheses())
-
-    def generic_programming_get_function_call_parentheses() -> str:
-        '''returns parentheses with the function call parentheses style'''
-        parentheses_container = get_parentheses_container()
-        stylized_parentheses = apply_spacing_style_to_container(function_call_parentheses_style.get(), parentheses_container)
-        return stylized_parentheses
-    def generic_programming_insert_function_call_parentheses():
-        '''inserts parentheses with the function call parentheses style'''
-        actions.insert(actions.user.generic_programming_get_function_call_parentheses())
-    def generic_programming_enter_function_call_parentheses_from_right():
-        '''enters the function call parentheses if called after inserting them'''
-        enter_stylized_container_from_right(actions.user.generic_programming_get_function_call_parentheses())
- 
- 
+    
 
     def generic_programming_build_for(beginning: str, separator_after_initialization: str, separator_after_condition: str, ending: str ):
         '''Builds a for loop using the code written on the current line'''

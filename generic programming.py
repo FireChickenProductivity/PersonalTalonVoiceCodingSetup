d7b50f9b935af7ba741b841c4f78078d24e0e911
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-08-04 17:38:23 -0600

    Added default generics actions

--- generic programming.py
+++ generic programming.py
@@ -171,6 +171,29 @@ class Actions:
         '''Does nothing intended to be overridden'''
         pass
 
+    def fire_chicken_programming_code_generics_operator():
+        '''Uses diamond symbol as default'''
+        diamond = get_declaration_angles()
+        diamond.insert()
+        diamond.enter_from_right()
+    def fire_chicken_programming_code_generic_type_start(typename: str):
+        '''By default, puts the typename in pascal case and then does the diamond operator'''
+        actions.user.insert_formatted(typename, 'hammer')
+        actions.user.fire_chicken_programming_code_generics_operator()
+    def fire_chicken_programming_code_generic_type_info_start(typename: str):
+        '''By default, inserts the diamond operator and puts the argument in it'''
+        actions.user.fire_chicken_programming_code_generic_type_info([typename])
+    def fire_chicken_programming_code_generic_type_info(typenames: List):
+        '''By default, inserts the diamond operator and puts the arguments in it separated by commas'''
+        actions.user.fire_chicken_programming_code_generics_operator()
+        is_first = True
+        for typename in typenames:
+            actions.user.insert_formatted(typename, 'hammer')
+            if is_first:
+                is_first = False
+            else:
+                actions.user.generic_programming_insert_comma_separator()
+
     def generic_programming_regular_expression_boundary_search_next(target: str):
         '''Searches for the desired text after a regular expression word boundary and goes to the left of it'''
         actions.edit.right()

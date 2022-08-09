2c93c80aa2be0d69cb3b3aa6e2f24d8392287f7a
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-08-09 15:39:17 -0600

    added talon python commands

---
+++ talon python.talon
@@ -0,0 +1,5 @@
+tag: user.python
+and tag: user.fire_chicken_talon_python_programming
+-
+keypress <user.keys>$: user.talon_python_programming_insert_keypress(keys)
+keypress <user.keys> over: user.talon_python_programming_insert_keypress(keys)
--- talon_programming.py
+++ talon_programming.py
@@ -1,8 +1,11 @@
-from talon import actions, Module
+from talon import actions, Module, Context
 
 from .styles import *
 
+talon_python_programming = Context()
+
 module = Module()
+module.tag('fire_chicken_talon_python_programming', desc = 'Activate talon python commands')
 @module.action_class
 class Actions:
     def talon_programming_insert_keypress(keys: str):
@@ -11,4 +14,21 @@ class Actions:
         function_call_parentheses = get_function_call_parentheses()
         function_call_parentheses.insert()
         function_call_parentheses.enter_from_right()
+        actions.insert(keys)
+
+    def talon_programming_activate_python_commands():
+        '''activates talon python commands'''
+        talon_python_programming.tags = ['user.fire_chicken_talon_python_programming']
+    def talon_programming_deactivate_python_commands():
+        '''deactivates talon python commands'''
+        talon_python_programming.tags = []
+    def talon_python_programming_insert_keypress(keys: str):
+        '''Inserts the talon command needed to make the key presses'''
+        actions.insert('actions.')
+        actions.insert('key')
+        function_call_parentheses = get_function_call_parentheses()
+        function_call_parentheses.insert()
+        function_call_parentheses.enter_from_right()
+        actions.insert("''")
+        actions.edit.left()
         actions.insert(keys)

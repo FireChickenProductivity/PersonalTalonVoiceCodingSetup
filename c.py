a9c2d09bfeebebb7b8ef0dce1b14364791e36430
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-01 10:55:35 -0700

    Add structure action

---
+++ c.py
@@ -0,0 +1,10 @@
+from talon import Module, actions
+
+module = Module()
+@module.action_class
+class Actions:
+    def fire_chicken_programming_insert_c_insert_structure(name: str):
+        ''''''
+        actions.insert('struct ')
+        actions.user.insert_formatted(name ,'hammer')
+        actions.user.c_style_programming_start_block()

39184008d657eba799acedb29e6df88de37ae5f8
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-11 15:48:25 -0700

    Add insert c constant functionality

--- c.py
+++ c.py
@@ -8,3 +8,9 @@ class Actions:
         actions.insert('struct ')
         actions.user.insert_formatted(name ,'hammer')
         actions.user.c_style_programming_start_block()
+    
+    def fire_chicken_programming_insert_c_constant(name: str):
+        ''''''
+        actions.insert('#define ')
+        actions.user.insert_formatted(name,'all cap,snake')
+        actions.key('space')

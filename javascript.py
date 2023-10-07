7cb580979b61a57370363ff9c7b6d006904671c5
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-10-07 10:06:04 -0600

    Add define react component action

--- javascript.py
+++ javascript.py
@@ -6,6 +6,15 @@ context.matches = r'''
 tag: user.javascript
 '''
 
+@module.action_class
+class Actions:
+    def fire_chicken_insert_react_component(name: str):
+        '''Creates the react component with specified name'''
+        component_name_text: str = actions.user.fire_chicken_convert_text_to_pascal_case(name)
+        component_start: str = f"<{component_name_text}>"
+        component_ending: str = f'</{component_name_text}>'
+        actions.user.fire_chicken_insert_around_cursor(component_start, component_ending)
+
 context.lists['user.fire_chicken_fast_functions'] = {
     'prompting' : 'prompt',
     'pieces' : '.forEach',

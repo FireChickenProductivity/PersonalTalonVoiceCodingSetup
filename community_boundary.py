3fb8fbb8dba3031d40bf91246d6a5f27c5506c87
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-10-07 10:02:45 -0600

    Add pascal case action

--- community_boundary.py
+++ community_boundary.py
@@ -8,3 +8,8 @@ class Actions:
         '''Inserts the text and adds it to the phrase history'''
         actions.insert(text)
         actions.user.add_phrase_to_history(text)
+
+    def fire_chicken_convert_text_to_pascal_case(text: str) -> str:
+        '''Converts the text to pascal case'''
+        return actions.user.formatted_text(text, "hammer")
+

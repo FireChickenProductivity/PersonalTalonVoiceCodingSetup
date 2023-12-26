0b216edc465300ee0d5b4869fdeca33d37bb9df5
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-25 19:27:54 -0700

    Remove commands slowing down talon

--- cursorless/operators/operators.py
+++
@@ -1,7 +0,0 @@
-from talon import Module
-
-module = Module()
-@module.capture(rule = 'ply')
-def fire_chicken_cursorless_operator_prefix(m) -> str:
-    '''A prefix said before an operator to combine operator and cursorless commands'''
-    return m[0]

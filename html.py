8c1e4626c89c110b5ced9bc65d60a1d248dcc5e3
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2024-05-27 13:38:14 -0600

    Add tag commands

--- html.py
+++ html.py
@@ -1,4 +1,5 @@
 from talon import Module, actions
+from typing import List
 
 module = Module()
 
@@ -21,3 +22,40 @@ class Actions:
     def fire_chicken_programming_html_insert_version_5_start():
         """Inserts the start of an HTML5 document"""
         actions.insert("<!DOCTYPE html>\n")
+
+    def fire_chicken_programming_html_insert_element(name: str):
+        """Inserts the tags for an HTML element"""
+        element_text = compute_multiline_element(name)
+        actions.insert(element_text)
+        actions.edit.line_up()
+    
+    def fire_chicken_programming_html_insert_single_line_element(name: str):
+        """Inserts the tags for a single line HTML element"""
+        element_text = compute_single_line_element(name)
+        actions.insert(element_text)
+        element_ending = compute_element_end(name)
+        for character in element_ending:
+            actions.edit.left()
+
+    def fire_chicken_programming_html_insert_single_line_element_from_list(characters: List[str]):
+        """Inserts the tags for a single line HTML element"""
+        name = "".join(characters)
+        actions.user.fire_chicken_programming_html_insert_single_line_element(name)
+        
+    def fire_chicken_programming_html_insert_header(header_number: int):
+        """Inserts HTML header tags"""
+        name = f"h{header_number}"
+        actions.user.fire_chicken_programming_html_insert_single_line_element(name)
+    
+
+def compute_element_start(name):
+    return f"<{name}>"
+
+def compute_element_end(name):
+    return f"</{name}>"
+
+def compute_single_line_element(name):
+    return f"<{name}></{name}>"
+
+def compute_multiline_element(name):
+    return f"<{name}>\n\n</{name}>"
--- html.talon
+++ html.talon
@@ -2,3 +2,5 @@ code.language: html
 -
 template start: user.fire_chicken_programming_html_insert_start_template()
 version five: user.fire_chicken_programming_html_insert_version_5_start()
+header <number_small>: user.fire_chicken_programming_html_insert_header(number_small)
+tagging <user.letter>+: user.fire_chicken_programming_html_insert_single_line_element_from_list(letter_list)
